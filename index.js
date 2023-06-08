588128a868c19e21197a24c7f44c9536c31ed9d3
RostraSvcUser <RostraSvcUser@transcarent.com>
2023-06-08 22:58:55 +0530

    minor

--- index.js
+++ index.js
@@ -21,16 +21,6 @@ app.get('/api/', (req, res) => {
 app.post('/api/convert', (req, res) => {
   try {
     const data = req.body;
-    // const data = [
-    //   {
-    //     id: '123',
-    //     html: '<p>Hello <span class="mention" data-index="1" data-denotation-char="@" data-id="83cadfb3-25f5-471e-a155-317eebffe090" data-value="Chirag Vaghasiya">﻿<span contenteditable="false"><span class="ql-mention-denotation-char">@</span>Chirag Vaghasiya</span>﻿</span>  and <span class="mention" data-index="2" data-denotation-char="@" data-id="e3c982ca-c5a1-4cf6-82e6-e98fb92a85e0" data-value="Chirag 123">﻿<span contenteditable="false"><span class="ql-mention-denotation-char">@</span>Chirag 123</span>﻿</span> </p>',
-    //   },
-    //   {
-    //     id: '123',
-    //     html: '<p>Test</p>',
-    //   },
-    // ];
     console.log('data',data);
     const temp = [];
     for (const iterator of data) {
